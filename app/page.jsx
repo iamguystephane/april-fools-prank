1196bbfb8fdfa759855dd0b8c8c89c1def73a6b4
iamguystephane <gstephane138@gmail.com>
2025-04-02 01:14:34 +0100

    done with the project, ready to host on vercel

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
-import { PartyPopper, Smile } from "lucide-react";
+import { PartyPopper, Smile, Sparkles } from "lucide-react";
+import './animation.css';
 
 export const metadata = {
   title: "April Fools",
@@ -7,7 +8,7 @@ export const metadata = {
 const HomePage = () => {
   return (
     <>
-      <div className="xsm:w-full md:w-2/3 lg:w-1/3 py-10 px-10 xsm:h-1/2 lg:h-3/4 flex flex-col align-center justify-center bg-white rounded-lg">
+      <div className="card xsm:w-full md:w-2/3 lg:w-1/3 py-2 xsm:px-14 xsm:h-fit lg:h-3/4 flex flex-col align-center justify-center bg-white rounded-lg">
         <div className="flex items-center justify-between w-full my-4 mt-10">
           <PartyPopper className="text-yellow-500" />
           <Smile className="text-red-500" />
@@ -22,16 +23,20 @@ const HomePage = () => {
           <p className="text-2xl text-black font-bold text-center">
             Guy Stephane :)
           </p>
-          <p className="text-sm text-red-500 flex-wrap text-center mb-3 font-bold">
-            (What you get when you're friends with a programmer!)
-          </p>
+          <div className='flex items-center justify-between w-full'>
+            <Sparkles className='star text-blue-500' />
+            <p className="text-sm text-red-500 flex-wrap text-center mb-3 font-bold">
+              (What you get when you're friends <br />with a programmer!)
+            </p>
+            <Sparkles className='star text-blue-500' />
+          </div>
           <p className="text-2xl font-bold text-black"> "I got you Good"</p>
           <p className="text-2xl font-bold text-black"> Happy new month </p>
         </div>
         <div className="flex items-center w-full gap-5 justify-center my-10">
-          <div className="w-[50px] h-[50px] rounded-full shadow-green-200 shadow-lg bg-yellow-400" />
-          <div className="w-[50px] h-[50px] rounded-full shadow-red-200 shadow-lg bg-green-400" />
-          <div className="w-[50px] h-[50px] rounded-full shadow-yellow-200 shadow-lg bg-blue-400" />
+          <div className="circle w-[50px] h-[50px] rounded-full shadow-green-200 shadow-lg bg-yellow-400" />
+          <div className="circle w-[50px] h-[50px] rounded-full shadow-red-200 shadow-lg bg-green-400" />
+          <div className="circle w-[50px] h-[50px] rounded-full shadow-yellow-200 shadow-lg bg-blue-400" />
         </div>
       </div>
     </>
